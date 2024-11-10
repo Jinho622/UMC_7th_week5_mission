101aa7db63b2450fca2b8b76e9b25645f5877a27
Jinho622 <aceho21@gmail.com>
2024-11-11 03:20:01 +0900

    feat(): 워크북 5주차 미션 완료

---
+++ src/main/java/UMC/UMC_7th_mission/Umc7thMissionApplication.java
@@ -0,0 +1,13 @@
+package UMC.UMC_7th_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc7thMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc7thMissionApplication.class, args);
+	}
+
+}
